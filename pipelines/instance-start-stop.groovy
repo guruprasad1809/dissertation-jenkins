3b790ec854ee4f07516375f92807bd33078b5f77
Guruprasad Hegde <guruprasadhegde18@gmail.com>
2022-10-22 23:03:33 +0530

    adding pipeline code

---
+++ pipelines/instance-start-stop.groovy
@@ -0,0 +1,30 @@
+node any {
+    if(Action == 'start')
+    {
+        stage ('Starting Instace')
+        {
+            echo "starting instance"
+            startinstace()
+        }
+    }
+    if(Action == 'stop')
+    {
+        stage ('Stopping Instance')
+        {
+            echo "Stopping Instace"
+            stopinstance()
+        }
+    }
+    if(Action == 'terminate')
+    {
+        stage('Terminating Instace')
+        {
+            echo "Terminating Instace"
+            terminateinstance()
+        }
+    }
+}
+def startinstace()
+{
+    sh (script:"aws ec2 start-instances --instance-ids ${instaceid.trim()}")
+}
