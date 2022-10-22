95a67bc688a38a59e24571df7cd0822034f90240
Guruprasad Hegde <guruprasadhegde18@gmail.com>
2022-10-22 21:14:29 +0530

    new code

--- jobs/instance-state.groovy
+++ jobs/instance-state.groovy
@@ -1,3 +1,22 @@
-jobs "instance-state-management" {
-    description
+job('example') {
+    publishers {
+        extendedEmail {
+            recipientList('me@halfempty.org')
+            defaultSubject('Oops')
+            defaultContent('Something broken')
+            contentType('text/html')
+            triggers {
+                beforeBuild()
+                stillUnstable {
+                    subject('Subject')
+                    content('Body')
+                    sendTo {
+                        developers()
+                        requester()
+                        culprits()
+                    }
+                }
+            }
+        }
+    }
 }
