99de6c8e3b929e7e1af7a3d9674f9a2ebef27cd5
Guruprasad Hegde <guruprasadhegde18@gmail.com>
2022-10-22 21:43:23 +0530

    adding pipeline code

--- jobs/instance-state.groovy
+++ jobs/instance-state.groovy
@@ -1,10 +1,16 @@
-pipeline {
-    agent any
-    stages {
-        stage('git repo & clean') {
-            steps {
-               echo "helllllllllllooooooooooooo"
-            }
+pipelineJob('instance-state-management') {
+    description('This pipeline is for instance state management')
+
+    parameters{
+
+        stringParam('Instance Id', instance_id , 'Please enter the Instance ids')
+        choiceParam('Action',['stop','start','terminate'], 'Please choose a desired action')
+    }
+
+    definition{
+        cps{
+            script(readFileFromWorkspace('pipelines/instance-start-stop.groovy'))
+            sandbox()
         }
     }
 }
